0004de87c02d8602aed59ccea06e7bb041aac24f
Staines-star <stainesjoy3152001@gmail.com>
2025-08-15 15:49:12 +0530

    Fix kotlin error and build issues

--- app/src/main/java/com/sj9/chavara/data/service/GcsImageLoader.kt
+++ app/src/main/java/com/sj9/chavara/data/service/GcsImageLoader.kt
@@ -1,49 +1,19 @@
 package com.sj9.chavara.data.service
 
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.BitmapFactory
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 
 /**
  * Service for loading images from Google Cloud Storage
  */
 class GcsImageLoader(private val context: Context) {
 
-    private val gcsService = GoogleCloudStorageService(context)
+
+
 
     /**
      * Load image bitmap from GCS URL
      */
-    suspend fun loadImageFromGcs(gcsUrl: String): Bitmap? = withContext(Dispatchers.IO) {
-        try {
-            if (!gcsUrl.startsWith("gs://")) {
-                return@withContext null
-            }
 
-            // Extract bucket and object path from GCS URL
-            val urlParts = gcsUrl.removePrefix("gs://").split("/", limit = 2)
-            if (urlParts.size != 2) {
-                return@withContext null
-            }
-
-            val bucketName = urlParts[0]
-            val objectPath = urlParts[1]
-
-            // Get image data from GCS
-            val imageData = gcsService.downloadFile(bucketName, objectPath)
-
-            if (imageData != null) {
-                BitmapFactory.decodeByteArray(imageData, 0, imageData.size)
-            } else {
-                null
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
-            null
-        }
-    }
 
     /**
      * Get public URL for GCS object (if bucket allows public access)
